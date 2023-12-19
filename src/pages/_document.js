28d00eb545281bad7c36ca8ea608e06514beb96e
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2023-12-19 20:32:38 +0000

    seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.cmsflush.com/" />
+        <meta property="og:title" content="CMSFlush" />
+        <meta property="og:description" content="A free mass deleter of Webflow CMS collection items" />
+        <meta property="og:image" content="https://scottadamson25.github.io/cmsflush/background.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
