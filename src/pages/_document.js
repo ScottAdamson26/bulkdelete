86f8e043d4e382aff20dd533cb0a28e699bf8f97
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2023-12-19 20:42:14 +0000

    analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.cmsflush.com/" />
         <meta property="og:title" content="CMSFlush" />
         <meta property="og:description" content="A free mass deleter of Webflow CMS collection items" />
         <meta property="og:image" content="https://scottadamson25.github.io/cmsflush/background.png" />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KR3FZ6680V"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KR3FZ6680V');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
