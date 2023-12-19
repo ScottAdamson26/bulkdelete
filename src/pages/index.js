21f38774864086bd8c9e5f036568b21e538f13e5
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2023-12-19 19:50:36 +0000

    update name and copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ export default function Page() {
     <main className="flex items-center justify-center min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
       {/* Hero Section */}
       <section className="text-center p-6 rounded-lg bg-white shadow-lg max-w-md mx-auto">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">Welcome to BulkDelete</h1>
-        <p className="text-gray-600 mb-6">Mass delete entire CMS collections with one click.</p>
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">Welcome to CMSFlush</h1>
+        <p className="text-gray-600 mb-6">Delete all items from a CMS collection with one click.</p>
 
         {/* CTA Button - Corrected Link Usage */}
         <Link href="/form" legacyBehavior>
